58de8c34a1211761465aade897743dc79c88482a
Svetlana <novoselovasv88@gmail.com>
2025-07-25 13:46:45 -0300

    Открытие paywall по клику.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleMenu = document.querySelector('#toggle_menu');
     const overlay = document.querySelector('#overlay');
+    const sidebar = document.querySelector('.sidebar');
+    const openPaywallElements = document.querySelectorAll('.paywall-show');
+    const paywall = document.querySelector('.card');
 
     toggleMenu.addEventListener('click', function () {
         overlay.classList.toggle('hidden');
+        sidebar.classList.toggle('hidden');
+    });
+
+    openPaywallElements.forEach(elem => {
+        elem.addEventListener('click', function () {
+            overlay.classList.remove('hidden');
+            paywall.classList.remove('hidden');
+            sidebar.classList.add('hidden');
+        });
     });
 });
